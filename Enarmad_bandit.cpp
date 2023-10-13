3259d9348ee2cc7577dab3718aed73040690b8de
Jonzoe <emilj03@gmail.com>
2023-10-13 20:15:52 +0200

    Added feature to create a gamefield and check win

--- Enarmad_bandit.cpp
+++ Enarmad_bandit.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 using namespace std;
 int depositMoney();
 int betMoney(int bet);
+bool creatFieldAndCheckWin();
 
 int main(){
     // Variables.
     int age, stake, totalWinToday = 0, playAgain, depositedMoney;
-    bool play = true;
+    bool win = false;
+
+    // Resets the time on the random function.
+    srand(time(0));
 
     // Welcomes the user.
     cout << "Welcome!\n"
             "How old are you?: ";
     cin >> age;
 
-    while(play == true){
+    while(true){
         // If the age of the user is 18 or above and wants to play again, they get to play.
         if (age >= 18){
             depositedMoney = depositMoney();
             stake = betMoney(depositedMoney);
 
-            int field[3][3];
-
-            for (int row = 0; row < 3; row++){
-                for (int square = 0; square < 3; square++){
-                    field[row][square] = rand() % 3;
-                    int character = field[row][square];
-                    switch(character){
-                        case 0:
-                            cout << "O ";
-                            break;
-                        case 1:
-                            cout << "X ";
-                            break;
-                        case 2:
-                            cout << "A ";
-                            break;
-                    }
-                }
-                cout << "\n\n";
+            if (creatFieldAndCheckWin()) {
+                cout << "Du har vunnit!\n";
             }
+            else {
+                cout << "Ingen vinst denna gång.\n";
+            }
+
             cout << "success, for now :(";
             break;
-
         }
         else{
             cout << endl;
@@ -69,7 +60,6 @@ int depositMoney(){
         cout << endl;
     } while (amountOfMoney != 100 && amountOfMoney != 300 && amountOfMoney != 500 && cout << "You can only choose between 100, 300 or 500 kr.");
 
-    cout << endl;
     cout << "You deposited " << amountOfMoney << " kr." << endl;
 
     return amountOfMoney;
@@ -91,4 +81,49 @@ int betMoney(int getDepositedMoney){
     } while (bet < 100 && cout << "You cannot bet under 100 kr." << endl || bet > getDepositedMoney && cout << "You cannot bet more than your deposit (" << getDepositedMoney << ") kr." << endl );
 
     return bet;
+}
+
+bool creatFieldAndCheckWin(){
+    // Creates an 2D array that is 3*3.
+    int field[3][3];
+
+    // Insers random characters into the array objects.
+    for (int row = 0; row < 3; row++){
+        for (int square = 0; square < 3; square++){
+            field[row][square] = rand() % 3;
+            int character = field[row][square];
+            switch(character){
+                case 0:
+                    cout << "O ";
+                    break;
+                case 1:
+                    cout << "X ";
+                    break;
+                case 2:
+                    cout << "A ";
+                    break;
+            }
+        }
+        cout << "\n";
+    }
+    
+    // Check win condition.
+    for (int i = 0; i < 3; i++) {
+        if (field[i][0] == field[i][1] && field[i][1] == field[i][2]) {
+            return true;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (field[0][i] == field[1][i] && field[1][i] == field[2][i]) {
+            return true;
+        }
+    }
+    if (field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+        return true;
+    } 
+    else if (field[0][2] == field[1][1] && field[1][1] == field[2][0]) {
+        return true;
+    }
+    return false;
 }
